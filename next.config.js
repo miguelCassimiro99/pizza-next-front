56473ca4ea449ec658cc29e698bab9f56ae9daa1
Miguel Cassimiro <miguels@umdoisesportes.com.br>
2022-12-13 22:17:48 -0300

    feat(tailwind): tailwind setup and login form

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  trailingSlash: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
 }
 
 module.exports = nextConfig
